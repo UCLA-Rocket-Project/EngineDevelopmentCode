055c1e80b4b745b31e089720d1d44e3f52a254cd
Alex C <acusianovic@gmail.com>
2018-10-26 01:09:35 -0700

    commented out some stuff

--- gas_orifice_flow_sizing.py
+++ gas_orifice_flow_sizing.py
@@ -8,11 +8,11 @@ HE_DynamicViscosity_DEFAULT = (2.10E-5 * u.Pa * u.s).to('lb / (ft * s)')
 N2_DynamicViscosity_DEFAULT = (19.84E-6 * u.Pa * u.s)
 def run():
     D = 0.25 * u.inch
-    DP = 80 * u.psi
+
     # rho = 2.11 * u.lb / u.ft ** 3 # density of helium at 3000 psi
     rho = 7.3935 * u.lb / u.ft ** 3
     Q = 0.336 * u.ft ** 3 / u.s
-    Y = 0.99
+
 
     Do = (fl.differential_pressure_meter_solver(D=D.to('m').magnitude,
                                           rho=rho.to('kg / m ** 3').magnitude,
@@ -23,6 +23,8 @@ def run():
                                           m=(Q * rho).to('kg / s').magnitude,
                                           meter_type='ISO 5167 orifice',
                                           taps='flange')*u.m).to('in')
+    # DP = 80 * u.psi
+    # Y = 0.99
     # guessarr = (0.5, 1.0, 0.2)
     # data = (D, DP, rho, Q, Y)
     # sol = sp.fsolve(equations, guessarr, args=data)
@@ -31,19 +33,20 @@ def run():
     print(str(Do))
     print('Done')
 
-def equations(p, *data):
-    D, DP, rho, Q, Y = data
-    C, Kf, d = p
-    g = 32.174 * u.ft / u.s ** 2
-    return (
-        C - fl.C_Reader_Harris_Gallagher(D=D.to('m').magnitude, Do=(d * u.inch).to('m').magnitude,
-                                         rho=rho.to('kg / m ** 3').magnitude,
-                                         mu=HE_DynamicViscosity_DEFAULT.to('Pa * s').magnitude,
-                                         m=(Q * rho).to('kg / s').magnitude,
-                                         taps='flange'),
-        Kf - (C / np.sqrt(np.abs((1 - (d / D.magnitude) ** 4)))),
-        Q.magnitude - (Kf * np.pi / 4 * (d * u.inch).to('ft') ** 2 * Y / 12 * np.sqrt(2 * g * DP.to('lbf / ft ** 2') / rho )).magnitude
-     )
+# Didn't work
+# def equations(p, *data):
+#     D, DP, rho, Q, Y = data
+#     C, Kf, d = p
+#     g = 32.174 * u.ft / u.s ** 2
+#     return (
+#         C - fl.C_Reader_Harris_Gallagher(D=D.to('m').magnitude, Do=(d * u.inch).to('m').magnitude,
+#                                          rho=rho.to('kg / m ** 3').magnitude,
+#                                          mu=HE_DynamicViscosity_DEFAULT.to('Pa * s').magnitude,
+#                                          m=(Q * rho).to('kg / s').magnitude,
+#                                          taps='flange'),
+#         Kf - (C / np.sqrt(np.abs((1 - (d / D.magnitude) ** 4)))),
+#         Q.magnitude - (Kf * np.pi / 4 * (d * u.inch).to('ft') ** 2 * Y / 12 * np.sqrt(2 * g * DP.to('lbf / ft ** 2') / rho )).magnitude
+#      )
 
 if __name__ == '__main__':
     run()
